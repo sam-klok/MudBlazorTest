b000a01c71c20d2a6d2be8c6a008068736cd5bde
sam-klok <sklok11@gmail.com>
2023-01-31 20:57:09 -0700

    Select all demo added

---
+++ MudBlazorTest/Pages/selectAllTest.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace MudBlazorTest.Pages
+{
+    public partial class SelectAllTest
+    {
+        private string value { get; set; } = "Nothing selected";
+        private IEnumerable<string> options { get; set; } = new HashSet<string>() { "Lion" };
+
+        private string[] felines =
+        {
+            "Jaguar", "Leopard", "Lion", "Lynx", "Panther", "Puma", "Tiger"
+        };
+
+        private string GetMultiSelectionText(List<string> selectedValues)
+        {
+            return $"{selectedValues.Count} feline{(selectedValues.Count > 1 ? "s have" : " has")} been selected";
+        }
+    }
+}
